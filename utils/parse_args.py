e54543772260776d053872ce67972f4f6c0fd679
Hong Guan <hguan6@asu.edu>
2024-06-25 16:53:10 -0700

    fixed bug in setting dataset for inter data deduplication

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -460,7 +460,12 @@ def parse_args():
         # model_args.n_original_weights = 163300953
     elif model_args.task_type == "vision_vit":
         model_args.model = "vit_large_patch16_224"
-        data_args.dataset_name = "CIFAR100" if not model_args.heter else "CelebA"
+        data_args.dataset_name = (
+            "CIFAR100"
+            if not model_args.heter
+            and not model_args.inter_data_mode == "cifar100_celeba"
+            else "CelebA"
+        )
         training_args.bs = 500
         training_args.mini_bs = 50
         model_args.block_size = 1048576 if not model_args.heter else 262144

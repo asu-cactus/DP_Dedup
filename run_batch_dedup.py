ca43ab5bc25e08fe1790c1f2922c7b46557f78ab
Hong Guan <hguan6@asu.edu>
2024-07-17 12:07:36 -0700

    print algorithm used

--- run_batch_dedup.py
+++ run_batch_dedup.py
@@ -1,12 +1,16 @@
 import os
 
 os.environ["WANDB_DISABLED"] = "true"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TQDM_DISABLE"] = "1"
 # from batch_dedup.every_n import run
+# print("Running every_n")
 
 # from batch_dedup.binary_search import run
+# print("Running binary_search")
 
 from batch_dedup.recursive_search_variant import run
+
+print("Running recursive_search_variant")
 
 run()

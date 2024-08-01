4b7046d52e1a01c4062b739701d0bd3d8510bf6f
Hong Guan <hguan6@asu.edu>
2024-08-01 16:46:02 -0700

    run vision task evaluation

--- plot_constitution.py
+++ plot_constitution.py
@@ -1,11 +1,12 @@
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 os.environ["WANDB_DISABLED"] = "true"
 import pdb
 from pathlib import Path
 import re
 from dataclasses import dataclass
+import time
 
 import numpy as np
 import seaborn as sns
@@ -116,11 +117,13 @@ def text_task_evaluate():
     Path(training_args.output_dir).mkdir(parents=True, exist_ok=True)
     models_info = load_models_info(model_args)
     for model_info in models_info:
+        tic = time.time()
         data_args.task_name = model_info["task_name"]
         print(f"Model: {model_info['model_path']}")
         model_args.model_name_or_path = model_info["model_path"]
         acc = evaluate(data_args, model_args, training_args, model_info)
         print(f"Accuracy: {acc}")
+        print(f"Time taken: {time.time() - tic}")
 
 
 def vision_task_parameters():
@@ -152,16 +155,19 @@ def vision_task_evaluation():
     from vision_task_utils.evaluate import evaluate
 
     model_args, data_args, training_args = parse_args()
-    models_info = load_models_info(model_args.task_type)
+    models_info = load_models_info(model_args)
     # model_paths = [info["model_path"] for info in models_info]
 
     for model_info in models_info:
+        tic = time.time()
         acc = evaluate(
             data_args,
             model_args,
-            model_info["model_path"],
+            training_args,
+            model_info,
         )
         print(f"Accuracy: {acc}")
+        print(f"Time taken: {time.time() - tic}")
 
 
 if __name__ == "__main__":
